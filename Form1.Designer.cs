ee88cf96fbd0298fbd206f56da9643cd4df217c3
Mai Xuân Ngọc <87193435+mxngocqb@users.noreply.github.com>
2022-11-07 19:26:54 +0700

    123

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,12 +43,17 @@
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.StopBits = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(160, 271);
+            this.label3.Location = new System.Drawing.Point(167, 286);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 12;
@@ -57,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 244);
+            this.label2.Location = new System.Drawing.Point(195, 259);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 13;
@@ -66,7 +71,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(224, 268);
+            this.comboBox2.Location = new System.Drawing.Point(231, 283);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 10;
@@ -74,10 +79,11 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(224, 241);
+            this.comboBox1.Location = new System.Drawing.Point(231, 256);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -101,7 +107,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(22, 268);
+            this.label5.Location = new System.Drawing.Point(29, 283);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 14;
@@ -110,7 +116,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 241);
+            this.label4.Location = new System.Drawing.Point(29, 256);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 15;
@@ -124,7 +130,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(365, 266);
+            this.button1.Location = new System.Drawing.Point(372, 281);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 26);
             this.button1.TabIndex = 17;
@@ -134,7 +140,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(364, 238);
+            this.button2.Location = new System.Drawing.Point(371, 253);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(76, 24);
             this.button2.TabIndex = 16;
@@ -145,7 +151,7 @@
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(224, 296);
+            this.comboBox3.Location = new System.Drawing.Point(231, 311);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 18;
@@ -153,17 +159,45 @@
             // StopBits
             // 
             this.StopBits.AutoSize = true;
-            this.StopBits.Location = new System.Drawing.Point(166, 296);
+            this.StopBits.Location = new System.Drawing.Point(173, 311);
             this.StopBits.Name = "StopBits";
             this.StopBits.Size = new System.Drawing.Size(52, 13);
             this.StopBits.TabIndex = 19;
             this.StopBits.Text = "Stop Bits:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(435, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 179);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(231, 335);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(195, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Cam:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 371);
+            this.ClientSize = new System.Drawing.Size(644, 368);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.StopBits);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label3);
@@ -176,10 +210,11 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Read COM";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +236,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Label StopBits;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,11 +13,20 @@ using System.IO.Ports;
 using System.Diagnostics;
 using System.Linq.Expressions;
 
+// Thu vien xu ly video
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Text.RegularExpressions;
+
 namespace Project_I
 {
     public partial class Form1 : Form
     {
-        string InputData = String.Empty; // Khai báo string buff dùng cho hiển thị dữ liệu sau này.
+        FilterInfoCollection cameras;
+        VideoCaptureDevice cam;
+        String InputData = String.Empty; // Khai báo string buff dùng cho hiển thị dữ liệu sau này.
+
+        public object SaveFileDialog1 { get; private set; }
 
         delegate void SetTextCallback(string text); // Khai bao delegate SetTextCallBack voi tham so string
         public Form1()
@@ -28,6 +37,11 @@ namespace Project_I
             comboBox2.Items.AddRange(BaudRate);
             string[] StopBits = { "1", "2", "0"};
             comboBox3.DataSource = StopBits;
+            cameras = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in cameras)
+            {
+                comboBox5.Items.Add(device.Name);
+            }
 
         }
 
@@ -50,6 +64,7 @@ namespace Project_I
         {
             comboBox1.DataSource = SerialPort.GetPortNames();// Quét các cổng COM đang hoạt động lên comboBox1
             comboBox2.SelectedIndex = 3;
+            comboBox5.SelectedIndex = 4;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -74,26 +89,38 @@ namespace Project_I
             if (InputData != String.Empty)
             {
                 //textbox1.Txt = InputData; // Ko dùng đc như thế này vì khác threads .
-                SetText(InputData); // Ủy quyền tại đây. Gọi delegate đã khai báo trước đó.
+                Solution(InputData); // Ủy quyền tại đây. Gọi delegate đã khai báo trước đó.
+                
             }
         }
-        private void SetText(string text)
+        private void Solution(string text)
         {
             if (this.textBox1.InvokeRequired)
             {
-                SetTextCallback d = new SetTextCallback(SetText); // khởi tạo 1 delegate mới gọi đến SetText
+                SetTextCallback d = new SetTextCallback(Solution); // khởi tạo 1 delegate mới gọi đến SetText
                 this.Invoke(d, new object[] { text });
             }
-            else this.textBox1.Text += text;
+            else
+            {
+                string except ="\n";
+                this.textBox1.Text += Regex.Replace(text, @"[^a-zA-Z0-9]+except+", string.Empty); // Loại bỏ các kí tự đặc biệt
+                saveFileDialog1.InitialDirectory = "D:\\HUST\\Project I\\src\\Project-I\\Data";
+                //SaveFileDialog1.DefaultExt = ".jpg";
+                saveFileDialog1.FileName = Regex.Replace(text, @"[^a-zA-Z0-9]", string.Empty) +".jpg"; // Lưu hình ảnh với tên file = ID thẻ
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+                }
+            }
 
         }
 
         // Conect với Serial Port I
         private void button2_Click(object sender, EventArgs e)
         {
+            // Ket noi cong COM
             if (!serialPort1.IsOpen)
             {
-
                 serialPort1.PortName = comboBox1.Text;
                 serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
                 switch (comboBox3.Text)
@@ -114,16 +141,41 @@ namespace Project_I
                 serialPort1.StopBits = System.IO.Ports.StopBits.One;
                 serialPort1.Open();
             }
+            // Ket noi Camera
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+            cam = new VideoCaptureDevice(cameras[comboBox5.SelectedIndex].MonikerString);
+            cam.NewFrame += Cam_NewFrame;
+            cam.Start();
         }
+
+        // hien thi noi dung camera len picture box
+        private void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bitmap;
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            serialPort1.Close(); 
+            serialPort1.Close(); // ngat ket noi COM
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop(); // Ngat ket noi camera
+            }
         }
 
         private void label6_Click_1(object sender, EventArgs e)
         {
             
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
